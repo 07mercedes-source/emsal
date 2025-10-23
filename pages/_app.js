bb26084438b68e1df31b4a7b2409205b207c7dbc
07mercedes-source <07mercedes@gmail.com>
2025-10-23 21:26:31 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,31 @@
 // pages/_app.js
 import dynamic from "next/dynamic";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
 import { AuthProvider } from "../context/AuthContext";
 import { IKProvider } from "../context/IKContext";
 import { DepoProvider } from "../context/DepoContext";
 import { RestaurantProvider } from "../context/RestaurantContext";
-import { LanguageProvider } from "../context/LanguageContext"; // 👈 Eksik olan eklendi
+import { LanguageProvider } from "../context/LanguageContext";
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  if (!router.isReady) return null;
+
   return (
     <AuthProvider>
-      <LanguageProvider> {/* 👈 Ekledik */}
-        <IKProvider>
-          <DepoProvider>
-            <RestaurantProvider>
+      <IKProvider>
+        <DepoProvider>
+          <RestaurantProvider>
+            <LanguageProvider>
               <Layout>
                 <Component {...pageProps} />
               </Layout>
-            </RestaurantProvider>
-          </DepoProvider>
-        </IKProvider>
-      </LanguageProvider>
+            </LanguageProvider>
+          </RestaurantProvider>
+        </DepoProvider>
+      </IKProvider>
     </AuthProvider>
   );
 }
