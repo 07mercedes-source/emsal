e27b5a2c0627a24c117b172092dc19d288ea6662
07mercedes-source <07mercedes@gmail.com>
2025-10-23 21:39:14 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 // pages/_app.js
-import dynamic from "next/dynamic";
 import "../styles/globals.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import Layout from "../components/Layout";
 import { AuthProvider } from "../context/AuthContext";
 import { IKProvider } from "../context/IKContext";
 import { DepoProvider } from "../context/DepoContext";
 import { RestaurantProvider } from "../context/RestaurantContext";
 import { LanguageProvider } from "../context/LanguageContext";
-import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
-  // Router hazır olmadan hiçbir şey render etme
-  if (!router || !router.isReady) return null;
+  // isReady değişkenini kullanmak yerine doğrudan router kontrolü
+  if (!router?.isReady) return null;
 
   return (
     <AuthProvider>
@@ -32,5 +32,4 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-// SSR’ı kapatıyoruz ki Vercel client-side renderda hata vermesin
 export default dynamic(() => Promise.resolve(MyApp), { ssr: false });
