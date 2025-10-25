c8c8661815ae6af83894a5cff6176a8ffca92f05
07mercedes-source <07mercedes@gmail.com>
2025-10-25 14:54:24 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // true bazen useRouter timing hatalarına yol açabiliyor
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
