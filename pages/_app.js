3e78c1054d5cf52b643d33a329c07a1bb520de9a
07mercedes-source <07mercedes@gmail.com>
2025-10-25 21:40:57 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
-import "@/styles/globals.css";
-import { AuthProvider } from "../context/AuthContext";
-import { DepoProvider } from "../context/DepoContext";
+// pages/_app.js
+import dynamic from "next/dynamic";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
+import { AuthProvider } from "../context/AuthContext";
+import { LanguageProvider } from "../context/LanguageContext";
+import { IKProvider } from "../context/IKContext";
+import { DepoProvider } from "../context/DepoContext";
+import { RestaurantProvider } from "../context/RestaurantContext";
+
+import Layout from "../components/Layout";
+
+function MyApp({ Component, pageProps }) {
+  // Tamamen client-side çalıştığı için SSR kaynaklı hatalar ortadan kalkar
   return (
     <AuthProvider>
-      <DepoProvider>
-        <Component {...pageProps} />
-      </DepoProvider>
+      <LanguageProvider>
+        <IKProvider>
+          <DepoProvider>
+            <RestaurantProvider>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </RestaurantProvider>
+          </DepoProvider>
+        </IKProvider>
+      </LanguageProvider>
     </AuthProvider>
   );
 }
+
+// SSR kapatıyoruz (Vercel/Next prerender sorunları için)
+export default dynamic(() => Promise.resolve(MyApp), { ssr: false });
