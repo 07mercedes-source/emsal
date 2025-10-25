7a8de6818979352124c2e270db3a5f3fb03c0f32
07mercedes-source <07mercedes@gmail.com>
2025-10-25 21:28:28 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,13 @@
-// pages/_app.js
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
-import { LanguageProvider } from "../context/LanguageContext";
 import { DepoProvider } from "../context/DepoContext";
-import { IKProvider } from "../context/IKContext";
-import { RestaurantProvider } from "../context/RestaurantContext";
-import Layout from "../components/Layout";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <LanguageProvider>
-        <RestaurantProvider>
-          <DepoProvider>
-            <IKProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </IKProvider>
-          </DepoProvider>
-        </RestaurantProvider>
-      </LanguageProvider>
+      <DepoProvider>
+        <Component {...pageProps} />
+      </DepoProvider>
     </AuthProvider>
   );
 }
