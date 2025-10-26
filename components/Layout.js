126696e2a19b3f1ec4dbd89947f2cce2f1312c27
07mercedes-source <07mercedes@gmail.com>
2025-10-26 19:27:31 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -6,9 +6,11 @@ export default function Layout({ children }) {
   return (
     <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}>
       <Navbar />
-      <div style={{ display: "flex", flex: 1 }}>
-        <main style={{ flex: 1, padding: 20, maxWidth: 1200, margin: "0 auto" }}>{children}</main>
-        <aside className="sidebar-right" style={{ background: "transparent" }}>
+      <div style={{ display: "flex", alignItems: "flex-start" }}>
+        <main style={{ flex: 1 }}>
+          <div className="container">{children}</div>
+        </main>
+        <aside style={{ width: 300, padding: 12 }}>
           <RightPanel />
         </aside>
       </div>
