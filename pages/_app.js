cbb6bf0d7d1f15659e1e48b7daac15b5aed8a08f
07mercedes-source <07mercedes@gmail.com>
2025-10-26 09:59:02 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,28 @@
-import "../styles/globals.css";
-import { AuthProvider } from "../context/AuthContext";
-import { LanguageProvider } from "../context/LanguageContext";
-import Navbar from "../components/Navbar";
+import "@/styles/globals.css";
+import { LanguageProvider } from "@/context/LanguageContext";
+import { DepoProvider } from "@/context/DepoContext";
+import Navbar from "@/components/Navbar";
+import RightPanel from "@/components/RightPanel";
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
-      <AuthProvider>
-        <Navbar />
-        <Component {...pageProps} />
-      </AuthProvider>
+      <DepoProvider>
+        <div className="flex min-h-screen">
+          <div className="flex-1 flex flex-col">
+            <Navbar />
+            <main className="flex flex-1">
+              <div className="flex-1 p-4">
+                <Component {...pageProps} />
+              </div>
+              <RightPanel />
+            </main>
+            <footer className="text-center text-gray-500 py-3 border-t">
+              © 2025 Emsal Dashboard
+            </footer>
+          </div>
+        </div>
+      </DepoProvider>
     </LanguageProvider>
   );
 }
