845e0dca076df77f571376013e86ff53644c44de
07mercedes-source <07mercedes@gmail.com>
2025-10-26 09:34:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,15 @@
-// pages/_app.js
-import dynamic from "next/dynamic";
 import "../styles/globals.css";
 import { AuthProvider } from "../context/AuthContext";
-import { IKProvider } from "../context/IKContext";
-import { DepoProvider } from "../context/DepoContext";
-import { RestaurantProvider } from "../context/RestaurantContext";
 import { LanguageProvider } from "../context/LanguageContext";
-import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 
-function MyApp({ Component, pageProps }) {
-  // SSR kapalı: client-side render ile hydration problemlerini engelliyoruz
+export default function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <LanguageProvider>
-        <IKProvider>
-          <DepoProvider>
-            <RestaurantProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </RestaurantProvider>
-          </DepoProvider>
-        </IKProvider>
-      </LanguageProvider>
-    </AuthProvider>
+    <LanguageProvider>
+      <AuthProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </LanguageProvider>
   );
 }
-
-export default dynamic(() => Promise.resolve(MyApp), { ssr: false });
